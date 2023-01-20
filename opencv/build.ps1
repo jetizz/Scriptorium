05ce59997b996aed480fe86575f158397cc35330
Kresimir Jelic <kresimir.jelic@microblink.com>
2023-01-20 17:13:42 +0100

    Version cleanup

--- opencv/build.ps1
+++ opencv/build.ps1
@@ -1,1 +1,2 @@
-docker buildx build -f dotnet7-cv4.7.0.Dockerfile --push --platform "linux/arm64,linux/amd64" --tag jetiz/ubuntu20-dotnet6-opencv4.7.0:202301 .
+#docker buildx build -f ubuntu20-dotnet7-cv4.5.3.Dockerfile --push --platform "linux/arm64,linux/amd64" --tag jetiz/ubuntu20-dotnet6-opencv4.5.3:202301 .
+#docker buildx build -f ubuntu22-dotnet7-cv4.7.0.Dockerfile --push --platform "linux/arm64,linux/amd64" --tag jetiz/ubuntu22-dotnet6-opencv4.7.0:202301 .
